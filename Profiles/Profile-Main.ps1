792f44e9c5177c248a0fd231b4c65a59233e9cd4
HardoMX <hardo314@gmail.com>
2023-05-15 10:54:02 +0200

    Added repos and included choice

--- Profiles/Profile-Main.ps1
+++ Profiles/Profile-Main.ps1
@@ -1,16 +1,27 @@
-Set-Location ~\Documents\GitHub\Code
-
 oh-my-posh init pwsh | Invoke-Expression
 
-function Code1 {
-    Set-Location ~\Documents\GitHub\Code
-}
-function Code2 {
-    Set-Location ~\Documents\GitHub\Code2
-}
-function profile {
-    Set-Location ~\Documents\PowerShell
-}
+function Code1 {Set-Location ~\Documents\GitHub\Code}
+function Code2 {Set-Location ~\Documents\GitHub\Code2}
+function profile {Set-Location ~\Documents\PowerShell}
+function Powershell {Set-Location ~\Documents\GitHub\PowerShell}
+function Hypr {Set-Location ~\Documents\GitHub\Hyprland-Config}
+
+do 
+{
+    Write-Host "Choose Reposiotry: 1) Code1. 2) Code2. 3) Profile. 4) PowerShell. 5) Hypr"
+    $Repo = Read-Host [Enter Selection]
+
+    Switch ($Repo)
+    {
+        "1" {Code1}
+        "2" {Code2}
+        "3" {profile}
+        "4" {Powershell}
+        "5" {Hypr}
+    }
+}until (1..5 -contains $Repo)
+
+Clear-Host
 
 Set-Alias -name stats -Value "~\Documents\GitHub\PowerShell\Scripts\Show-Usage.ps1"
 Set-Alias -name make -Value "~\Documents\GitHub\PowerShell\Scripts\make.ps1"

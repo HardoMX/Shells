90c8c809293cab82e323d5a878e86b3571947481
HardoMX <hardo314@gmail.com>
2023-05-22 20:10:33 +0200

    Add Alias for nvim

--- Profiles/Profile-Main.ps1
+++ Profiles/Profile-Main.ps1
@@ -5,10 +5,11 @@ function Code2 {Set-Location ~\Documents\GitHub\Code2}
 function profile {Set-Location ~\Documents\PowerShell}
 function Powershell {Set-Location ~\Documents\GitHub\PowerShell}
 function Hypr {Set-Location ~\Documents\GitHub\Hyprland-Config}
+function Nvim {Set-Location ~\Documents\GitHub\Nvim-Config}
 
 do 
 {
-    Write-Host "Choose Reposiotry: 1) Code1. 2) Code2. 3) Profile. 4) PowerShell. 5) Hypr"
+    Write-Host "Choose Reposiotry: 1) Code1. 2) Code2. 3) Profile. 4) PowerShell. 5) Hypr. 6) Nvim"
     $Repo = Read-Host [Enter Selection]
 
     Switch ($Repo)
@@ -18,10 +19,12 @@ do
         "3" {profile}
         "4" {Powershell}
         "5" {Hypr}
+        "6" {Nvim}
     }
-}until (1..5 -contains $Repo)
+}until (1..6 -contains $Repo)
 
 Clear-Host
 
 Set-Alias -name stats -Value "~\Documents\GitHub\PowerShell\Scripts\Show-Usage.ps1"
 Set-Alias -name make -Value "~\Documents\GitHub\PowerShell\Scripts\make.ps1"
+Set-Alias -name nvim -Value nvim.exe

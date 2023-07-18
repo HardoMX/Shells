d7c6790e1c6422f52beebc6abcbea683ae6b80d1
HardoMX <hardo314@gmail.com>
2023-07-18 03:16:51 +0200

    Improved ls command with lsd

--- bash/bash.sh
+++ bash/bash.sh
@@ -6,7 +6,15 @@ HISTSIZE= HISTFILESIZE= #Set History File to Infinite
 
 alias mkd="mkdir -pv"
 
-alias ll="ls -hNal --color=auto --group-directories-first"
-alias ls="ls -hN --color=auto --group-directories-first"
+if [ lsd ];
+then
+    alias ll="lsd -hal --group-directories-first"
+    alias ls="lsd -ha --group-directories-first"
+else
+    alias ll="ls -hNal --color=auto --group-directories-first" #See all files and folders in a list with full info
+    alias ls="ls -hN --color=auto --group-directories-first" #Nicer ls command
+fi
+alias grep="grep --color-auto"
+
 
 eval "$(oh-my-posh init bash --config $HOME/Shells/oh-my-posh/theme.omp.json)"

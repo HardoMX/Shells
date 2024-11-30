72df2e3c856de19d1bc0ed33f5fc485b7528185b
Hardo <andreas@andreashard.tech>
2024-11-30 15:29:04 +0100

    add check and path for bob, the nvim version manager

--- bash/bash.sh
+++ bash/bash.sh
@@ -7,7 +7,7 @@ alias mkd="mkdir -pv"
 alias cd..="cd .."
 alias cd...="cd ../.."
 
-if [ lsd ];
+if [ hash lsd 2>/dev/null ];
 then
     alias ll="lsd -hal --group-directories-first"
     alias ls="lsd -ha --group-directories-first"
@@ -20,14 +20,14 @@ alias grep="grep --color=auto"
 SUDO_EDITOR=/usr/bin/nvim
 export SUDO_EDITOR
 
-if [ oh-my-posh ];
+if [ hash oh-my-posh 2>/dev/null ];
 then
     # Choose between local and web-based config location. Local is faster, but web-based might be more up to date depending on your config
     # # eval "$(oh-my-posh init bash --config https://raw.githubusercontent.com/HardoMX/Shells/main/oh-my-posh/theme.omp.json)"
     eval "$(oh-my-posh init bash --config $HOME/Shells/oh-my-posh/theme.omp.json)"
 fi
 
-if [ git ];
+if [ hash git 2>/dev/null ];
 then
     alias gstat="git status"
     alias gadd="git add"
@@ -38,13 +38,17 @@ then
     alias gdiff="git diff"
 fi
 
-if [ fastfetch ];
+if [ hash fastfetch 2>/dev/null ];
 then
     alias neofetch="fastfetch"
 fi
 
-if [ gem ];
+if [ hash gem 2>/dev/null ];
 then
     export GEM_HOME="$(ruby -e 'puts Gem.user_dir')"
     export PATH="$PATH:$GEM_HOME/bin"
 fi
+
+if [ hash bob 2>/dev/null ];
+then
+    export PATH="$PATH:$HOME/.local/share/bob/nvim-bin"
